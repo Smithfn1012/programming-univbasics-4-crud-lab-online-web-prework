fd95da9261b37ceb2906fb7d12d1691ad9772309
Felicia Smith <smithfn1012@gmail.com>
2019-11-23 18:21:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,34 +7,32 @@ def create_an_array
   ["alternative", "rock", "indie", "soul"]
 end
 
-
-
 def add_element_to_end_of_array(array, element) 
-  favorite_music_genres << "techno-pop"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  favorite_music_genres.unshift("80's")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  favorite_music_genres.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  favorite_music_genres.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  favorite_music_genres.index("rock")
+ return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  favorite_music_genres[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  favorite_music_genres[-1]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
